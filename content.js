2ff3b18ce905eecf49186909cd096d7ef0525140
Luciano Ratamero <luciano@ratamero.com>
2018-05-02 15:59:14 -0300

    adds initial extension

---
+++ content.js
@@ -0,0 +1,24 @@
+
+function disableAutoplay(){
+  var autoplaySection = document.getElementById('autoplay');
+
+  if (!autoplaySection) {
+    // since youtube is loaded dinamically,
+    // we need to wait until the element is there.
+    return setTimeout(function(){ disableAutoplay(); }, 500);
+  }
+
+  var autoplayToggle = autoplaySection.nextElementSibling;
+
+  if (!autoplayToggle) {
+    // since youtube is loaded dinamically,
+    // we need to wait until the element is there.
+    return setTimeout(function(){ disableAutoplay(); }, 500);
+  }
+
+  if (autoplayToggle.getAttribute('aria-pressed') === "true") {
+    autoplayToggle.click();
+  }
+}
+
+disableAutoplay();
