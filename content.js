cae37cc9dc593479e752923eb2dee2538acec32c
Luciano Ratamero <luciano@ratamero.com>
2018-06-15 18:21:16 -0300

    updates script to make it compatible with old youtube versions

--- content.js
+++ content.js
@@ -1,18 +1,26 @@
 
 function disableAutoplay(){
-  var autoplaySection = document.getElementById('autoplay');
+  var autoplaySection = document.getElementById('autoplay'),
+      autoplayCheckbox = document.getElementById('autoplay-checkbox');
 
-  if (!autoplaySection) {
+  if (!autoplaySection && !autoplayCheckbox) {
     // since youtube is loaded dinamically,
     // we need to wait until the element is there.
     return setTimeout(function(){ disableAutoplay(); }, 500);
   }
 
-  var autoplayToggle = autoplaySection.nextElementSibling;
+  if (autoplaySection) {
 
-  if (autoplayToggle.getAttribute('aria-pressed') === "true") {
-    autoplayToggle.click();
-    autoplayToggle.nextElementSibling.click();
+    var autoplayToggles = document.getElementsByTagName('paper-toggle-button');
+    for (var index = 0; index < autoplayToggles.length; index++) {
+      var toggle = autoplayToggles[index];
+      if (toggle.getAttribute('aria-pressed') === "true") {
+        toggle.click();
+      }
+    }
+
+  } else if (autoplayCheckbox && autoplayCheckbox.checked) {
+    autoplayCheckbox.click();
   }
 }
 
