20868f59bed61a58c20dce321988dc40377300cc
Luciano Ratamero <luciano@ratamero.com>
2021-04-28 18:45:53 -0300

    Fix non-responsive buttons on firefox

--- content.js
+++ content.js
@@ -1,35 +1,68 @@
+var MAX_RETRIES = 20;
+
 function disableAutoplay() {
   disableAutoplayV2();
   disableAutoplayV3();
 }
 
-function disableAutoplayV3() {
-  var autoplayButton = document.querySelector('[aria-label="Autoplay is on"]');
+function disableAutoplayV3(retryCount) {
+  if (!retryCount) {
+    retryCount = 0;
+  }
 
-  if (!autoplayButton) {
+  if (retryCount >= MAX_RETRIES) {
+    // probably not in v3 or already disabled
+    return;
+  }
+
+  var autoplayButton = document.querySelector(
+    ".ytp-autonav-toggle-button[aria-checked=true]"
+  );
+
+  if (document.readyState !== "complete") {
     // since youtube is loaded dinamically,
     // we need to wait until the element is there.
     // this operation is really cheap,
     // so it shouldn't impact performance/battery.
     return setTimeout(function () {
-      disableAutoplay();
-    }, 500);
+      disableAutoplayV3();
+    }, 5000);
+  } else if (autoplayButton) {
+    // somehow, on firefox, we need
+    // to wait a bit more for the button to be interactive
+    return setTimeout(function () {
+      autoplayButton.click();
+    }, 1500);
   }
 
-  autoplayButton.click();
+  return setTimeout(function () {
+    disableAutoplayV3(retryCount + 1);
+  }, 500);
 }
 
-function disableAutoplayV2() {
+function disableAutoplayV2(retryCount) {
+  if (!retryCount) {
+    retryCount = 0;
+  }
+
+  if (retryCount >= MAX_RETRIES) {
+    // probably not in v2 or already disabled
+    return;
+  }
+
   var autoplaySection = document.getElementById("autoplay"),
     autoplayCheckbox = document.getElementById("autoplay-checkbox");
 
-  if (!autoplaySection && !autoplayCheckbox) {
+  if (
+    document.readyState !== "complete" ||
+    !(autoplaySection && autoplayCheckbox)
+  ) {
     // since Youtube is loaded dynamically,
     // we need to wait until the element is there.
     // this operation is really cheap,
     // so it shouldn't impact performance/battery.
     return setTimeout(function () {
-      disableAutoplay();
+      disableAutoplayV2(retryCount + 1);
     }, 500);
   }
 
